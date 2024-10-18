94987932a8a65931f7ca5ad893ac966d9b54394b
sepfy <git@mail.fuzzball.enterprises>
2024-10-18 09:25:45 -0400

    Update Terraform proxmox to v0.66.2

--- terraform/rancher/providers.tf
+++ terraform/rancher/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source = "bpg/proxmox"
-      version = "0.55.0"
+      version = "0.66.2"
     }
     #proxmox = {
     #  source = "MaartendeKruijf/proxmox"
